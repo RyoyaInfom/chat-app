51b8dff4316553e9364c3938e98387a1c7bcdcb3
RyoyaInfom <r.nakanouchi@infomart.co.jp>
2025-12-01 10:56:22 +0900

    ユーザー認証機能の実装

---
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -0,0 +1,70 @@
+package in.tech_camp.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import in.tech_camp.chat_app.entity.UserEntity;
+import in.tech_camp.chat_app.form.LoginForm;
+import in.tech_camp.chat_app.form.UserForm;
+import in.tech_camp.chat_app.repository.UserRepository;
+import in.tech_camp.chat_app.service.UserService;
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+
+
+
+@Controller
+@AllArgsConstructor
+public class UserController {
+  
+  private final UserRepository userRepository;
+  private final UserService userService;
+
+
+  @GetMapping("/users/sign_up")
+  public String showSignUp(Model model) {
+      model.addAttribute("userForm",new UserForm());
+      return "users/signUp";
+  }
+
+  @PostMapping("/user")
+  public String createUser(@ModelAttribute("userForm") UserForm userForm, Model model) {
+      UserEntity userEntity = new UserEntity();
+      userEntity.setName(userForm.getName());
+      userEntity.setEmail(userForm.getEmail());
+      userEntity.setPassword(userForm.getPassword());
+
+      try{
+        userService.createUserWithEncryptedPassword(userEntity);
+      }catch(Exception e){
+        System.out.println("エラー:" + e);
+        return "/users/signUp";
+      }
+      
+      return "redirect:/";
+  }
+
+  @GetMapping("/users/login")
+  public String LoginUser(Model model) {
+    model.addAttribute("loginForm", new LoginForm());
+      return "users/login";
+  }
+  
+  @GetMapping("/login")
+  public String showLoginWithError(@RequestParam(value = "error")String error, Model model) {
+      if(error != null){
+        model.addAttribute("loginError","invalid email or password.");
+      }
+      return("users/login");
+  }
+  
+  
+}
