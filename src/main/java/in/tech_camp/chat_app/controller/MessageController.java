b5261e1d427133d569670227ee965ea93fab2ee4
RyoyaInfom <r.nakanouchi@infomart.co.jp>
2025-12-02 15:40:32 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import in.tech_camp.chat_app.repository.MessageRepository;
 import in.tech_camp.chat_app.repository.RoomRepository;
 import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
-import in.tech_camp.chat_app.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
 
 
@@ -71,9 +69,10 @@ public class MessageController {
 
   @PostMapping("/rooms/{roomId}/messages")
   public String createMessage(@PathVariable("roomId") Integer roomId, 
-  @ModelAttribute("messageForm") @Validated(ValidationOrder.class) MessageForm messageForm, 
+  @ModelAttribute("messageForm") MessageForm messageForm, 
   BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser) {
       
+      messageForm.ValidateMessage(bindingResult);
       if(bindingResult.hasErrors()){
         return "redirect:/rooms/" + roomId + "/messages";
       }
--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,5 +1,6 @@
 package in.tech_camp.chat_app.form;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 
 import in.tech_camp.chat_app.validation.ValidationPriority1;
@@ -9,8 +10,14 @@ import lombok.Data;
 @Data
 public class MessageForm {
 
-  @NotBlank(message="Message is can't blank", groups=ValidationPriority1.class)
+  //@NotBlank(message="Message is can't blank", groups=ValidationPriority1.class)
   private  String content;
 
   private MultipartFile image;
+
+  public void ValidateMessage(BindingResult result){
+    if((content == null || content.isEmpty()) && (image == null || image.isEmpty())){
+      result.rejectValue("Content", "error.Message", "Please enter either content or image");
+    }
+  }
 }
